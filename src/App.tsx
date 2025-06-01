39c4c315ab5ef148cd215392559e0fdcf8a6640d
Vien Huynh <vien200203@gmail.com>
2025-06-02 00:33:46 +0700

    config(posthog): add posthog analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import EventPage from "./pages/EventPage";
-import NotFound from "./pages/NotFound";
+import { Toaster as Sonner } from '@/components/ui/sonner';
+import { Toaster } from '@/components/ui/toaster';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import EventPage from './pages/EventPage';
+import Index from './pages/Index';
+import NotFound from './pages/NotFound';
 
 const queryClient = new QueryClient();
 
@@ -18,9 +17,9 @@ const App = () => {
         <Sonner />
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/event/:id" element={<EventPage />} />
-            <Route path="*" element={<NotFound />} />
+            <Route path='/' element={<Index />} />
+            <Route path='/event/:id' element={<EventPage />} />
+            <Route path='*' element={<NotFound />} />
           </Routes>
         </BrowserRouter>
       </TooltipProvider>
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { PostHogProvider } from 'posthog-js/react';
 
-createRoot(document.getElementById("root")!).render(<App />);
+const options = {
+  api_host: import.meta.env.VITE_POSTHOG_HOST,
+};
+
+createRoot(document.getElementById('root')!).render(
+  <PostHogProvider apiKey={import.meta.env.VITE_POSTHOG_KEY} options={options}>
+    <App />
+  </PostHogProvider>
+);
